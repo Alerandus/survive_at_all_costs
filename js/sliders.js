d433f57139c45dc48c8d4bf01f388a49f8c734a0
Alerandus <maximryzen@gmail.com>
2025-04-18 21:53:10 +0700

    + второй секшион

--- js/sliders.js
+++ js/sliders.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.first-section__swiper', {
     loop: true, // Бесконечный цикл
     slidesPerView: 1, // Один слайд на весь экран
     spaceBetween: 0, // Пробел между слайдами
@@ -7,4 +7,27 @@ const swiper = new Swiper('.swiper', {
       disableOnInteraction: false, // Не отключать автопрокрутку при взаимодействии
     },
     allowTouchMove: false,
+  });
+
+
+  const swiper1 = new Swiper('.second-section__swiper', {
+    slidesPerView: 1, // Один слайд на весь экран
+    spaceBetween: 0, // Пробел между слайдами
+    speed: 150,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    effect: 'coverflow',
+
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 440,
+      slideShadows: true,
+    },
   });
